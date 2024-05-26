9deb95bc4f9550a74156701142b08fe4a8c46a10
Nikita Fomin <97741120+rixelraze@users.noreply.github.com>
2024-05-26 23:48:01 +0300

    Update GymManagementSystem.java

--- gymmanagementsystem/src/main/java/com/example/gymmanagementsystem/GymManagementSystem.java
+++ gymmanagementsystem/src/main/java/com/example/gymmanagementsystem/GymManagementSystem.java
@@ -12,37 +12,45 @@ import java.util.Objects;
 
 public class GymManagementSystem extends Application {
 
+    // Поля для хранения начальных координат мыши
     private double x = 0;
     private double y = 0;
 
     @Override
     public void start(Stage stage) throws Exception {
+        // Загрузка FXML-документа и создание корневого узла сцены
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("FXMLDocument.fxml")));
 
+        // Создание новой сцены с корневым узлом
         Scene scene = new Scene(root);
 
-        root.setOnMousePressed((MouseEvent event) ->{
-            x = event.getSceneX();
-            y = event.getSceneY();
+        // Обработчик события нажатия мыши
+        root.setOnMousePressed((MouseEvent event) -> {
+            x = event.getSceneX(); // Сохранение координаты X при нажатии
+            y = event.getSceneY(); // Сохранение координаты Y при нажатии
         });
 
-        root.setOnMouseDragged((MouseEvent event) ->{
-            stage.setX(event.getScreenX() - x);
-            stage.setY(event.getScreenY() - y);
+        // Обработчик события перемещения мыши
+        root.setOnMouseDragged((MouseEvent event) -> {
+            stage.setX(event.getScreenX() - x); // Установка новой позиции окна по X
+            stage.setY(event.getScreenY() - y); // Установка новой позиции окна по Y
 
-            stage.setOpacity(.8);
+            stage.setOpacity(.8); // Изменение прозрачности окна при перемещении
         });
 
-        root.setOnMouseReleased((MouseEvent event) ->{
-            stage.setOpacity(1);
+        // Обработчик события отпускания мыши
+        root.setOnMouseReleased((MouseEvent event) -> {
+            stage.setOpacity(1); // Восстановление прозрачности окна
         });
 
+        // Установка прозрачного стиля окна
         stage.initStyle(StageStyle.TRANSPARENT);
 
+        // Установка сцены для окна
         stage.setScene(scene);
+        // Отображение окна
         stage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
